00120c5f205d3cffdd2dee2edf9044473a42ebf3
IamSanketMore <sanketmore.999.m@gmail.com>
2021-03-17 00:11:34 +0530

    Test Case for check data are successful retrive from database

---
+++ test/java/EmployeePayRollServiceTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class EmployeePayRollServiceTest
+{
+
+    @Test
+    void given3EmpWhenWrittenToFilesShouldMatchEmpEntries()
+    {
+        EmployeePayrollData[] arrayOfEmp= {
+                new EmployeePayrollData(1,"Sanket",1235),
+                new EmployeePayrollData(2,"Bill",1235),
+                new EmployeePayrollData(3,"Mark",1235),
+        };
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmp));
+        employeePayrollService.empWriteData(EmployeePayrollService.IOService.FILE_IO);
+        employeePayrollService.printData(EmployeePayrollService.IOService.FILE_IO);
+        long result = employeePayrollService.countEntries(EmployeePayrollService.IOService.FILE_IO);
+        System.out.println("Total Employee Entries :- "+result);
+        Assertions.assertEquals(3,result);
+
+
+    }
+
+    @Test
+    public void givenEmployeePayrollDB_WhenRetrieved_ShouldMatchEmpCount() {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayRollData(EmployeePayrollService.IOService.DB_IO);
+        System.out.println(employeePayrollData);
+        Assertions.assertEquals(6, employeePayrollData.size());
+    }
+}
